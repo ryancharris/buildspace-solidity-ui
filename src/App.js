e820514b75bf0806034c0d3d281073a0c46d3b72
Ryan Harris <harrisryan1@gmail.com>
2021-09-11 14:11:13 -0400

    update project files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { ethers } from "ethers";
+// import { ethers } from "ethers";
 import './App.css';
 
 export default function App() {
 
   const wave = () => {
-    
+    console.log('wave')
   }
   
   return (
@@ -13,11 +13,12 @@ export default function App() {
 
       <div className="dataContainer">
         <div className="header">
-        👋 Hey there!
+        <span role="img" aria-label="wave">👋</span>
+        Hey there!
         </div>
 
         <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+        I am Ryan. Connect your Ethereum wallet and wave at me!
         </div>
 
         <button className="waveButton" onClick={wave}>
