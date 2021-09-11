d427dd1946b53f57562c4932683f881c3e72f4b8
Ryan Harris <harrisryan1@gmail.com>
2021-09-11 15:41:54 -0400

    add contract functionality, ui states

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import './App.css';
+import abiFile from './utils/WavePortal.json'
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(false)
+  const [hasLoaded, setHasLoaded] = useState(false)
+  const [minedHash, setMinedHash] = useState(null)
   const [currentAcct, setCurrentAcct] = useState("");
+  const [numOfWaves, setNumOfWaves] = useState(0);
 
   useEffect(() => {
     checkWalletConnection()
+    getPreviousWaveCount()
   }, []);
+
+  const getPreviousWaveCount = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner();
+    const waveportalContract = new ethers.Contract('0xe821DB566ef664D70CD3043f3779D92528D8189C', abiFile.abi, signer)
+
+    let count = await waveportalContract.getTotalWaves()
+    setNumOfWaves(count.toNumber())
+  }
 
   const checkWalletConnection = () => {
     const { ethereum } = window;
@@ -46,9 +61,38 @@ export default function App() {
     .catch(err => console.log(err))
   }
 
-  const wave = () => {
-    console.log('wave')
+  const wave = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner();
+    const waveportalContract = new ethers.Contract('0xe821DB566ef664D70CD3043f3779D92528D8189C', abiFile.abi, signer)
+
+    let count = await waveportalContract.getTotalWaves();
+    console.log('Retrieved total wave count...', count.toNumber());
+
+    const waveTxn = await waveportalContract.wave();
+    console.log('Mining...', waveTxn.hash);
+    setIsLoading(true);
+    setMinedHash(waveTxn.hash)
+
+    await waveTxn.wait();
+    console.log('Mined -- ', waveTxn.hash);
+    setIsLoading(false);
+    setHasLoaded(true);
+
+    setTimeout(() => {
+      setHasLoaded(false);
+    }, 2500);
+
+    count = await waveportalContract.getTotalWaves();
+    console.log('Retrieved total wave count...', count.toNumber());
+    setNumOfWaves(count.toNumber())
   }
+
+  const connectBtn = currentAcct ? null : (
+    <button className="waveButton" onClick={connectWallet}>
+      Connect Wallet
+    </button>
+  )
   
   return (
     <div className="mainContainer">
@@ -60,17 +104,39 @@ export default function App() {
         </div>
 
         <div className="bio">
-        I am Ryan. Connect your Ethereum wallet and wave at me!
+          I am Ryan. Connect your Ethereum wallet and wave at me!
         </div>
 
-        <button className="waveButton" onClick={wave}>
-          Wave at Me
-        </button>
+        {isLoading && (
+          <div>
+            <p>Mining...</p>
+            <p>{minedHash}</p>
+          </div>
+        )}
 
-        {currentAcct ? null : (
-          <button className="waveButton" onClick={connectWallet}>
-            Connect Wallet
-          </button>
+        {hasLoaded && (
+          <div>
+            <p>Mined...</p>
+            <p>{minedHash}</p>
+          </div>
+        )}
+
+        {!isLoading && (
+          <div>
+            <p style={{textAlign: "center",}}>
+              I've already been waved at {numOfWaves} times!
+            </p>
+          </div>
+        )}
+
+        {!isLoading && (
+          <>
+            <button className="waveButton" onClick={wave}>
+              Wave at Me
+            </button>
+
+            {connectBtn}
+          </>
         )}
       </div>
     </div>
