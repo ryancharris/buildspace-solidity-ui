1bcbf52cdf2e5bee67fe70a2596d414de08f16f6
Ryan Harris <harrisryan1@gmail.com>
2021-09-11 17:43:33 -0400

    finish section 3

--- src/App.js
+++ src/App.js
@@ -5,20 +5,41 @@ import abiFile from './utils/WavePortal.json'
 
 export default function App() {
   const [isLoading, setIsLoading] = useState(false)
-  const [hasLoaded, setHasLoaded] = useState(false)
   const [minedHash, setMinedHash] = useState(null)
   const [currentAcct, setCurrentAcct] = useState("");
   const [numOfWaves, setNumOfWaves] = useState(0);
+  const [allWaves, setAllWaves] = useState([])
+  const [messageInput, setMessageInput] = useState("");
+  console.log(messageInput)
 
   useEffect(() => {
     checkWalletConnection()
     getPreviousWaveCount()
   }, []);
 
+  const getAllWaves = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner();
+    const waveportalContract = new ethers.Contract('0x2ECDF70FB8Ea0dA36d567535f56636faEf9438F4', abiFile.abi, signer)
+
+    let waves = await waveportalContract.getAllWaves();
+    let wavesCleaned = [];
+    
+    waves.forEach(wave => {
+      wavesCleaned.push({
+        address: wave.waver,
+        timestamp: new Date(wave.timestamp * 1000),
+        message: wave.message
+      })
+    })
+
+    setAllWaves(wavesCleaned)
+  }
+
   const getPreviousWaveCount = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum)
     const signer = provider.getSigner();
-    const waveportalContract = new ethers.Contract('0xe821DB566ef664D70CD3043f3779D92528D8189C', abiFile.abi, signer)
+    const waveportalContract = new ethers.Contract('0x2ECDF70FB8Ea0dA36d567535f56636faEf9438F4', abiFile.abi, signer)
 
     let count = await waveportalContract.getTotalWaves()
     setNumOfWaves(count.toNumber())
@@ -34,12 +55,14 @@ export default function App() {
     }
 
     ethereum.request({ method: 'eth_accounts' })
-    .then(accounts => {
+    .then(async accounts => {
       if (accounts.length !== 0) {
         const account = accounts[0]
         console.log('Found an authorized account: ', account)
         
         setCurrentAcct(account)
+
+        getAllWaves();
       } else {
         console.log('No authorized account')
       }
@@ -64,12 +87,12 @@ export default function App() {
   const wave = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum)
     const signer = provider.getSigner();
-    const waveportalContract = new ethers.Contract('0xe821DB566ef664D70CD3043f3779D92528D8189C', abiFile.abi, signer)
+    const waveportalContract = new ethers.Contract('0x2ECDF70FB8Ea0dA36d567535f56636faEf9438F4', abiFile.abi, signer)
 
     let count = await waveportalContract.getTotalWaves();
     console.log('Retrieved total wave count...', count.toNumber());
 
-    const waveTxn = await waveportalContract.wave();
+    const waveTxn = await waveportalContract.wave(messageInput);
     console.log('Mining...', waveTxn.hash);
     setIsLoading(true);
     setMinedHash(waveTxn.hash)
@@ -77,11 +100,7 @@ export default function App() {
     await waveTxn.wait();
     console.log('Mined -- ', waveTxn.hash);
     setIsLoading(false);
-    setHasLoaded(true);
-
-    setTimeout(() => {
-      setHasLoaded(false);
-    }, 2500);
+    getAllWaves();
 
     count = await waveportalContract.getTotalWaves();
     console.log('Retrieved total wave count...', count.toNumber());
@@ -98,46 +117,74 @@ export default function App() {
     <div className="mainContainer">
 
       <div className="dataContainer">
-        <div className="header">
-        <span role="img" aria-label="wave">👋</span>
-        Hey there!
-        </div>
+        <h1 className="header" style={{marginTop: 0}}>
+          <span role="img" aria-label="wave">👋</span>
+          Hey there!
+        </h1>
 
         <div className="bio">
           I am Ryan. Connect your Ethereum wallet and wave at me!
         </div>
 
         {isLoading && (
-          <div>
+          <div style={{textAlign: 'center'}}>
             <p>Mining...</p>
             <p>{minedHash}</p>
           </div>
         )}
 
-        {hasLoaded && (
-          <div>
-            <p>Mined...</p>
-            <p>{minedHash}</p>
-          </div>
-        )}
-
         {!isLoading && (
-          <div>
+          <>
             <p style={{textAlign: "center",}}>
               I've already been waved at {numOfWaves} times!
             </p>
-          </div>
-        )}
 
-        {!isLoading && (
-          <>
-            <button className="waveButton" onClick={wave}>
+            <input type="text" value={messageInput} onChange={(event) => {
+              setMessageInput(event.target.value);
+            }}
+            style={{
+              margin: '8px 0',
+              padding: '12px',
+            }}
+            />
+            <button className="waveButton" onClick={() => {
+              setMessageInput('');
+              wave();
+            }}>
               Wave at Me
             </button>
 
             {connectBtn}
           </>
         )}
+
+        <div style={{marginTop: '36px'}}>
+          <h2>Message Log:</h2>
+          {allWaves
+            .sort((a, b) => {
+              if (a.timestamp < b.timestamp) {
+                return 1;
+              } else {
+                return -1;
+              }
+            })
+            .map(wave => {
+            return (
+              <div style={{
+                border: '1px solid black',
+                borderRadius: '4px',
+                boxShadow: '2px 1.5px rgba(0, 0, 0, 0.15)',
+                padding: '8px 12px',
+                margin: '0 0 16px 0'
+              }}>
+                <p><strong>Address:</strong> {wave.address}</p>
+                <p><strong>Timestamp:</strong> {wave.timestamp.toString()}</p>
+                <p><strong>Message:</strong> {wave.message}</p>
+              </div>
+            )
+          })}
+        </div>
+
       </div>
     </div>
   );
